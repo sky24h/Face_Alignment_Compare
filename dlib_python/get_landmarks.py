309d226fc116c7572eee8fb151daa7723b4a8331
Sky24H <hytian2@gmail.com>
2021-09-27 05:23:34 +0900

    update

--- dlib_python/get_landmarks.py
+++ dlib_python/get_landmarks.py
@@ -4,13 +4,15 @@ import cv2
 
 
 def get_landmarks(frame_list):
+    face_detector = dlib.get_frontal_face_detector()
     predictor_path = './dlib_python/shape_predictor_68_face_landmarks.dat'
     face_predictor = dlib.shape_predictor(predictor_path)
-
-    dlibRect = dlib.rectangle(0, 0, 256, 256)
+    shape = frame_list[0].shape[1]
+    dlibRect = dlib.rectangle(0, 0, shape, shape)
     landmarks = []
     for frame in frame_list:
-        landmark = face_predictor(frame, dlibRect)
+        faces = face_detector(frame, 1)
+        landmark = face_predictor(frame, faces[0])
         landmark = face_utils.shape_to_np(landmark)
         landmarks.append(landmark)
 
